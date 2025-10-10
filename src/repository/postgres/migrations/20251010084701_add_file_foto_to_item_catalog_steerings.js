491c0b9c3b927f99279303da2dcebe3aa64ceb38
abdulfalaq5 <abdulfalaq5@gmail.com>
2025-10-10 16:41:45 +0700

    FIx: issue migrasi

--- src/repository/postgres/migrations/20251010084701_add_file_foto_to_item_catalog_steerings.js
+++ src/repository/postgres/migrations/20251010084701_add_file_foto_to_item_catalog_steerings.js
@@ -3,7 +3,9 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.table('item_catalog_steerings', function(table) {
+        table.text('file_foto').nullable().comment('URL or path to the uploaded foto file');
+      });
 };
 
 /**
@@ -11,5 +13,7 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+    return knex.schema.table('item_catalog_steerings', function(table) {
+        table.dropColumn('file_foto');
+      });
 };
